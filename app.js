c5c154293edf20a20103a73fc6e5cbc417a22dd4
t-aono <info@t-aono.net>
2022-07-16 14:26:08 +0900

    make image upload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var http = require("http");
 var express = require("express");
 var path = require("path");
-var bodyparser = require("body-parser");
 var mongoose = require("mongoose");
+var fileUpload = require("express-fileupload");
 
 var Message = require("./schema/Message");
 
@@ -16,10 +16,12 @@ mongoose.connect("mongodb://localhost:27017/chat", function (err) {
   }
 });
 
-app.use(bodyparser());
+app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+app.use("/image", express.static(path.join(__dirname, "image")));
 
 app.get("/", function (req, res) {
   Message.find({}, function (err, msgs) {
@@ -32,15 +34,30 @@ app.get("/update", function (req, res, next) {
   return res.render("update");
 });
 
-app.post("/update", function (req, res, next) {
-  var newMessage = new Message({
-    username: req.body.username,
-    message: req.body.message,
-  });
-  newMessage.save((err) => {
-    if (err) throw err;
-    return res.redirect("/");
-  });
+app.post("/update", fileUpload(), function (req, res, next) {
+  if (req.files && req.files.image) {
+    req.files.image.mv("./image/" + req.files.image.name, function (err) {
+      if (err) throw err;
+      var newMessage = new Message({
+        username: req.body.username,
+        message: req.body.message,
+        image_path: "/image/" + req.files.image.name,
+      });
+      newMessage.save((err) => {
+        if (err) throw err;
+        return res.redirect("/");
+      });
+    });
+  } else {
+    var newMessage = new Message({
+      username: req.body.username,
+      message: req.body.message,
+    });
+    newMessage.save((err) => {
+      if (err) throw err;
+      return res.redirect("/");
+    });
+  }
 });
 
 var server = http.createServer(app);
